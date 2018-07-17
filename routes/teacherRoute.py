2af0a6a7a5b5e011bc8d78e6af66be5a94e43b55
Anthony Harlan <harlan_a@4j.lane.edu>
2018-07-17 15:17:43 -0700

    working on grid

--- routes/teacherRoute.py
+++ routes/teacherRoute.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect, url_for, render_template 
+from flask import Flask, redirect, url_for, render_template, session
 import os.path
 import sys
 
 #viewSwitch = {"about"}
 #sys.path.insert(0,'./routes/')
+
 
 class TeacherRoute:
 	html=""
@@ -16,16 +17,24 @@ class TeacherRoute:
 		elif len(args)==1:
 			if args[0]=='verify':
 				self.html=render_template(self.affiliation+'Verify.html')
-			elif args[0]=='upload':
-				self.html=render_template(self.affiliation+'Upload.html')
 			elif args[0]=='uploadsuccess':
 				self.html=render_template(self.affiliation+'UploadSuccess.html')
 			elif args[0]=='success':
 				self.html=render_template(self.affiliation+'Success.html')
 		elif len(args)==2:
-			self.body=self.loadBody("<B>Error: </B>"+args[0], "<B>Message: </B>"+args[1])
+			if args[0]=='upload':
+				session = args[1]
+				username = session['FIRST_NAME'][0]+" "+session["LAST_NAME"][0]
+				school = session['organizationName'][0]
+				submitToken = session['submitToken']
+				requestId = session['requestId']
+				self.html=render_template(self.affiliation+'Upload.html', username=username, school=school, submitToken=submitToken, requestId=requestId)
+			else:
+				self.html=render_template(self.affiliation+'Verify.html')
+				html=html.replace("<!--Errors go here-->","<B>Error: </B>"+args[0]+"<BR><B>Message: </B>"+args[1])
 		else:
-			self.body=self.loadBody("<B>Error: </B>Invalid arguments", "<B>Message: </B>"+[msg for msg in args])
+			self.html=render_template(self.affiliation+'Verify.html')
+			html=html.replace("<!--Errors go here-->","<B>Error: </B>Invalid arguments"+"<BR><B>Message: </B>"+[msg+", " for msg in args])
 
 	app = Flask(__name__, template_folder='../views/partials')
 
